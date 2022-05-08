a64f233681aa90ebdb327b7acb317e96d86c3c3a
Hiranya Rathnayake <87335419+HiranyaDilukshi@users.noreply.github.com>
2022-05-08 16:00:47 +0530

    Update server.js

--- CAREFIRST/pharmacymanagement/server.js
+++ CAREFIRST/pharmacymanagement/server.js
@@ -6,6 +6,8 @@ const app=express();
 const cors=require('cors');
 
 const empRoutes= require('./routes/employees');
+const Router = require('./routes/stockitems');
+const CartRouter =require('./routes/itemcart');
 
 
 //app middleware
@@ -13,6 +15,14 @@ const empRoutes= require('./routes/employees');
  app.use(cors());
 
  app.use(empRoutes);
+ app.use(Router);
+app.use(CartRouter);
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({ extended: true })
+);
 
 const PORT = 8000;
 
@@ -29,63 +39,3 @@ app.listen(PORT, () =>{
  
 
 });
- 
-const express = require('express');
-const mongoose = require('mongoose');
-
-
-
-
-//convert request to java script object
-const bodyParser = require('body-parser'); 
-
-const cors = require("cors");
-
-
-const app=express();
-
-
-//import node.js pacakage for providing a connect/Express middleware can used to enable Cors with options
-app.use(cors());
-
-//import routes
-const Router = require('./routes/stockitems');
-const CartRouter =require('./routes/itemcart');
-
-//app middleware
-app.use(bodyParser.json());
-
-
-
-
-//routes middleware
-app.use(Router);
-app.use(CartRouter);
-
-
-app.use(express.json());
-
-app.use(
-  express.urlencoded({ extended: true })
-);
-
-
-
-const PORT=8000;
-const DB_URL = 'mongodb+srv://pwn:12345@myfirstcluster.9gnms.mongodb.net/carefirst?retryWrites=true&w=majority';
-mongoose.connect(DB_URL,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() =>{
-   console.log('DB connected');
-})
-.catch((err) => console.log('DB connection ERROR',err));
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`App is running on http://localhost:${PORT}`);
-});
-
- 
