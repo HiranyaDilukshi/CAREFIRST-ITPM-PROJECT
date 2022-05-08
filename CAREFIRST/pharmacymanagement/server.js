d3e7d5a23206ce46bd0e82854b370cded2922b70
Hiranya Rathnayake <87335419+HiranyaDilukshi@users.noreply.github.com>
2022-05-08 19:45:06 +0530

    Update server.js

--- CAREFIRST/pharmacymanagement/server.js
+++ CAREFIRST/pharmacymanagement/server.js
@@ -1,34 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-
-
-
-//convert request to java script object
-const bodyParser = require('body-parser'); 
-
-const cors = require("cors");
-
-
+ 
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyparser=require('body-parser');
 const app=express();
+const cors=require('cors');
 
-//import node.js pacakage for providing a connect/Express middleware can used to enable Cors with options
-app.use(cors());
-
-//import routes
-const empRoutes= require('./routes/supplier');
+const empRoutes= require('./routes/employees');
+const Router = require('./routes/stockitems');
+const CartRouter =require('./routes/itemcart');
 const recRoutes= require('./routes/receive');
+const supRoutes= require('./routes/supplier');
 
 
 //app middleware
-app.use(bodyParser.json());
-app.use(empRoutes);
+ app.use(bodyparser.json());
+ app.use(cors());
+
+ app.use(empRoutes);
+ app.use(Router);
+app.use(CartRouter);
+app.use(supRoutes);
 app.use(recRoutes);
-
-
-
-//routes middleware
-
 
 app.use(express.json());
 
@@ -36,22 +28,20 @@ app.use(
   express.urlencoded({ extended: true })
 );
 
+const PORT = 8000;
 
+const DB_URL= 'mongodb+srv://pwn:12345@myfirstcluster.9gnms.mongodb.net/carefirst?retryWrites=true&w=majority'
 
-const PORT=8000;
-const DB_URL = 'mongodb+srv://pwn:12345@myfirstcluster.9gnms.mongodb.net/carefirst?retryWrites=true&w=majority';
-mongoose.connect(DB_URL,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect(DB_URL)
+.then(()=>{
+    console.log("DB connected")
 })
-.then(() =>{
-   console.log('DB connected');
-})
-.catch((err) => console.log('DB connection ERROR',err));
+.catch((err)=>console.log('DB connection error',err))
 
+app.listen(PORT, () =>{
+ console.log(`App is running on ${PORT}`);
+ 
 
-
-app.listen(PORT, ()=>{
-    console.log(`App is running on http://localhost:${PORT}`);
 });
+
 
